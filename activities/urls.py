a587b2c57642155494f4d214743bd31a4521f06c
safaamostafa <safamos21@gmail.com>
2024-05-09 19:43:57 +0300

    fix exam score url

--- activities/urls.py
+++ activities/urls.py
@@ -11,6 +11,6 @@ router.register(r'clinics-attendance', ClinicViewSet)
 router.register(r'shifts-attendance', ShiftAttendanceViewSet)
 router.register(r'operations-attendance', OperationAttendanceViewSet)
 router.register(r'exams', ExamViewSet, basename='exam')
-router.register(r'examscore', ExamScoreViewSet, basename='examscore')
+router.register(r'exam-scores', ExamScoreViewSet, basename='examscore')
 
 urlpatterns = router.urls
