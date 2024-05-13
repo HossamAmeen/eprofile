6ba6bad57ba3cf496a4425a02e7ea6ef1df25833
hossam ameen <hossam.ameen@shipblu.com>
2024-05-13 17:13:00 +0300

    add isort

--- activities/api.py
+++ activities/api.py
@@ -19,6 +19,7 @@ from activities.serializer import (ClinicAttendanceSerializer,
 from notifications.models import ActivityNotification
 from users.models import Student
 
+
 class LectureViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Lecture.objects.order_by('-id')
