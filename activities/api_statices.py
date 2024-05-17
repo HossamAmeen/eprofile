c94b976a8993b7ffcbf7ea7fac1acbfcea6058ae
Atareq <tarekahmad8509@gmail.com>
2024-05-16 20:16:10 -0700

    staffmember_statistices_first_try

--- activities/api_statices.py
+++ activities/api_statices.py
@@ -1,10 +1,22 @@
-from django.db.models import Func, IntegerField
-from .models import StaffMember
+from django.db.models import Count, Q
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
+from .models import StaffMember
 
 @api_view(['GET'])
-def calculate_statistices(request):
-    pass
+def calculate_statistics(request):
+
+    staff_members = StaffMember.objects.annotate(
+        action_nums=Count('studentactivity', filter=~Q(
+            studentactivity__approve_status='pending')))
+
+    response_data = [
+        {
+            'staff_member_id': staff_member.id,
+            'staff_member_name': staff_member.full_name,
+            'action_nums': staff_member.action_nums
+        }
+        for staff_member in staff_members
+    ]
+    return Response(response_data, status=status.HTTP_200_OK)
--- activities/urls.py
+++ activities/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .api_statistices import calculate_statistices
+from .api_statices import calculate_statistics
 from rest_framework.routers import DefaultRouter
 from activities.api import (ClinicViewSet, ExamScoreViewSet, ExamViewSet,
                             LectureViewSet, OperationAttendanceViewSet,
@@ -15,12 +15,11 @@ router.register(r'operations-attendance', OperationAttendanceViewSet)
 router.register(r'exams', ExamViewSet, basename='exams')
 router.register(r'exam-scores', ExamScoreViewSet, basename='exam-scores')
 
-# helllo hossssam
-
 
 urlpatterns = router.urls
 urlpatterns += [
     path('student-statistics/', StudentActivityStatisticAPIView.as_view()),
-    path('statistices-staff-members/', calculate_statistices),
-    path('update/exam-score/', ExamScoreViewSet.as_view({"patch": "bulk_update"})),
+    path('statistices-staff-members/', calculate_statistics),
+    path('update/exam-score/', ExamScoreViewSet.as_view({
+        "patch": "bulk_update"})),
 ]
