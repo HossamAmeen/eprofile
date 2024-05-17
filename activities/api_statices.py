883290adbb03db2c43b6654125d14a2b5e1ee784
Atareq <tarekahmad8509@gmail.com>
2024-05-16 21:13:25 -0700

    sratistices

--- activities/api_statices.py
+++ activities/api_statices.py
@@ -2,6 +2,7 @@ from django.db.models import Count, Q
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .models import StaffMember
 
 
