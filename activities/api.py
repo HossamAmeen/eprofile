c8548b21333ceaf96a56aec61d41dde8e4f1cc4b
Atareq <tarekahmad8509@gmail.com>
2024-05-19 03:35:14 -0700

    model_fix

--- activities/api.py
+++ activities/api.py
@@ -137,11 +137,6 @@ class ExamViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['competence_level']
 
-    def get_queryset(self):
-        if self.request.user.get_role() == 'student':
-            return self.queryset.filter(student=self.request.user.id)
-        return self.queryset
-
     def get_serializer_class(self):
         if self.request.method == "GET":
             return ListExamSerializer
