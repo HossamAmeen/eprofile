c51498126a63e8499c269509dabec4f34dcdbcaa
Atareq <97070266+Atareq@users.noreply.github.com>
2024-05-20 11:10:35 -0700

    Fix/model calculate statistices query (#59)
    * solve_urls_for_staff_statistic
    
    * sec_try_urls
    
    * sec_try_urls_and_api_file
    
    * staffmember_statistices_first_try
    
    * staffmember_statistices_first_try
    
    * staff_statistices
    
    * flake8_chek
    
    * flake8_chek2
    
    * rename score_image fields in StudentActivity model
    
    * rename score_image fields in StudentActivity model
    
    * model_fix
    
    * model_fix
    
    * model_fix
    
    * fix_migration_files
    
    * fix_migration_files
    
    * remove unused
    
    ---------
    
    Co-authored-by: hossam ameen <hosamameen948@gmail.com>

--- activities/models.py
+++ activities/models.py
@@ -18,6 +18,8 @@ class StudentActivity(models.Model):
     staff_member = models.ForeignKey(StaffMember, null=True,
                                      on_delete=models.SET_NULL)
     student = models.ForeignKey(Student, null=True, on_delete=models.SET_NULL)
+    score_image = models.TextField(null=True, blank=True)
+    score_image_2 = models.TextField(null=True, blank=True)
 
 
 class Lecture(StudentActivity):
