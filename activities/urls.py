6b87154d8ae659c4cd596c6afb04d8cdfcdab980
hossam ameen <hosamameen948@gmail.com>
2024-05-22 23:02:39 +0300

    fix isort

--- activities/urls.py
+++ activities/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from activities.api import (StaffMemberStatisticsAPIView, ClinicViewSet,
-                            ExamScoreViewSet, ExamViewSet,
+from activities.api import (ClinicViewSet, ExamScoreViewSet, ExamViewSet,
                             LectureAttendanceViewSet, LectureViewSet,
                             OperationAttendanceViewSet, ShiftAttendanceViewSet,
+                            StaffMemberStatisticsAPIView,
                             StudentActivityStatisticAPIView)
 
 router = DefaultRouter()
