4e08f23cbbbd3142b6030a9571f39a06335f70b0
Atareq <tarekahmad8509@gmail.com>
2024-06-12 07:57:42 -0700

    feature/fixtures_for_testing

---
+++ activities/tests/staffmember_statistices_test.py
@@ -0,0 +1,38 @@
+import pytest
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
+
+
+@pytest.mark.django_db
+class TestActivity:
+
+    @pytest.fixture
+    def add_user(self):
+        user = User.objects.create(
+            full_name="original_name",
+            email="original_email@gmail.com",
+            phone="000",
+        )
+        return user
+
+    @pytest.fixture
+    def token(self, add_user):
+        refresh = RefreshToken.for_user(add_user)
+        return str(refresh.access_token)
+
+    @pytest.fixture
+    def api_client(self, token):
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+        return client
+
+    def test_staffstatistices(self, api_client):
+        url = reverse('staff-member-statistices')
+        response = api_client.get(url, content_type='application/json')
+        assert response.status_code == 200
+        response_data = response.json()
+        assert response_data['count'] == 1
--- users/tests/user_model_test.py
+++ users/tests/user_model_test.py
@@ -6,30 +6,34 @@ from users.models import CompetenceLevel, Student
 
 @pytest.mark.django_db
 class TestStudent:
-    def test_student_create(self, client):
-        url = reverse('students-list')
-        competence_level = CompetenceLevel.objects.create(name="Level 1")
-        data = {
-            "full_name": "hossam",
-            "email": "tareqsstudent@gmail.com",
-            "phone": "01010079798",
-            "birth_of_date": "1997-02-03",
-            "competence_level": competence_level.pk,
-            "password": "admin"
-        }
-        response = client.post(url, data, content_type='application/json')
-
-        assert Student.objects.count() == 1
-        assert response.status_code == 201
-
-    def test_student_update(self, client):
+    @pytest.fixture
+    def add_user(self):
         test_user = Student.objects.create(
             full_name="original_name",
             email="original_email@gmail.com",
             phone="000",
             birth_of_date="2000-01-01",
         )
-        url = reverse('students-detail', args=[test_user.id])
+        return test_user
+
+    def test_student_create(self, client):
+        Compentence_level = CompetenceLevel.objects.create(name="Level 1")
+        url = reverse('students-list')
+        data = {
+            "full_name": "hossam",
+            "email": "tareqsstudent@gmail.com",
+            "phone": "01010079798",
+            "birth_of_date": "1997-02-03",
+            "competence_level": Compentence_level.id,
+            "password": "admin"
+        }
+        response = client.post(url, data, content_type='application/json')
+
+        assert Student.objects.count() == 2
+        assert response.status_code == 201
+
+    def test_student_update(self, client, add_user):
+        url = reverse('students-detail', args=[add_user.id])
         data = {
             "full_name": "hossam",
             "email": "tareqsstudent@gmail.com",
@@ -37,37 +41,26 @@ class TestStudent:
             "birth_of_date": "1997-02-03",
         }
         response = client.patch(url, data, content_type='application/json')
-        test_user.refresh_from_db()
+        add_user.refresh_from_db()
         assert response.status_code == 200
-        assert test_user.full_name == "hossam"
-        assert test_user.email == "tareqsstudent@gmail.com"
-        assert test_user.phone == "123"
-        assert test_user.birth_of_date.strftime('%Y-%m-%d') == "1997-02-03"
+        assert add_user.full_name == "hossam"
+        assert add_user.email == "tareqsstudent@gmail.com"
+        assert add_user.phone == "123"
+        assert add_user.birth_of_date.strftime('%Y-%m-%d') == (
+            "1997-02-03")
 
     def test_student_list(self, client):
         url = reverse('students-list')
         response = client.get(url, content_type='application/json')
         assert response.status_code == 200
 
-    def test_student_retrive(self, client):
-        test_user = Student.objects.create(
-            full_name="original_name",
-            email="original_email@gmail.com",
-            phone="000",
-            birth_of_date="2000-01-01",
-        )
-        url = reverse('students-detail', args=[test_user.id])
+    def test_student_retrive(self, client, add_user):
+        url = reverse('students-detail', args=[add_user.id])
         response = client.get(url, content_type='application/json')
         assert response.status_code == 200
 
-    def test_student_delete(self, client):
-        test_user = Student .objects.create(
-            full_name="original_name",
-            email="original_email@gmail.com",
-            phone="000",
-            birth_of_date="2000-01-01",
-        )
-        url = reverse('students-detail', args=[test_user.id])
+    def test_student_delete(self, client, add_user):
+        url = reverse('students-detail', args=[add_user.id])
         response = client.delete(url)
         assert response.status_code == 204
-        assert not Student.objects.filter(pk=test_user.id).exists()
+        assert not Student.objects.filter(pk=add_user.id).exists()
