94b2d4351cfda6a5c25ab5224709e66f527d88d8
hossam ameen <hosamameen948@gmail.com>
2024-06-26 21:38:02 +0300

    remove unused changes

--- users/tests/user_model_test.py
+++ users/tests/user_model_test.py
@@ -31,9 +31,15 @@ class TestStudent:
 
         assert Student.objects.count() == 1
         assert response.status_code == 201
-        
-    def test_student_update(self, client, add_user):
-        url = reverse('students-detail', args=[add_user.id])
+
+    def test_student_update(self, client):
+        test_user = Student.objects.create(
+            full_name="original_name",
+            email="original_email@gmail.com",
+            phone="000",
+            birth_of_date="2000-01-01",
+        )
+        url = reverse('students-detail', args=[test_user.id])
         data = {
             "full_name": "hossam",
             "email": "tareqsstudent@gmail.com",
@@ -41,26 +47,37 @@ class TestStudent:
             "birth_of_date": "1997-02-03",
         }
         response = client.patch(url, data, content_type='application/json')
-        add_user.refresh_from_db()
+        test_user.refresh_from_db()
         assert response.status_code == 200
-        assert add_user.full_name == "hossam"
-        assert add_user.email == "tareqsstudent@gmail.com"
-        assert add_user.phone == "123"
-        assert add_user.birth_of_date.strftime('%Y-%m-%d') == (
-            "1997-02-03")
+        assert test_user.full_name == "hossam"
+        assert test_user.email == "tareqsstudent@gmail.com"
+        assert test_user.phone == "123"
+        assert test_user.birth_of_date.strftime('%Y-%m-%d') == "1997-02-03"
 
     def test_student_list(self, client):
         url = reverse('students-list')
         response = client.get(url, content_type='application/json')
         assert response.status_code == 200
 
-    def test_student_retrive(self, client, add_user):
-        url = reverse('students-detail', args=[add_user.id])
+    def test_student_retrive(self, client):
+        test_user = Student.objects.create(
+            full_name="original_name",
+            email="original_email@gmail.com",
+            phone="000",
+            birth_of_date="2000-01-01",
+        )
+        url = reverse('students-detail', args=[test_user.id])
         response = client.get(url, content_type='application/json')
         assert response.status_code == 200
 
-    def test_student_delete(self, client, add_user):
-        url = reverse('students-detail', args=[add_user.id])
+    def test_student_delete(self, client):
+        test_user = Student .objects.create(
+            full_name="original_name",
+            email="original_email@gmail.com",
+            phone="000",
+            birth_of_date="2000-01-01",
+        )
+        url = reverse('students-detail', args=[test_user.id])
         response = client.delete(url)
         assert response.status_code == 204
-        assert not Student.objects.filter(pk=add_user.id).exists()
+        assert not Student.objects.filter(pk=test_user.id).exists()
